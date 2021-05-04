c61d64c84ce49f6696850bc0461cd84b34e49118
Kat Sublett <38798134+katsublett@users.noreply.github.com>
2021-05-03 22:39:02 -0400

    feat: create constructor and print data function The class had no constructor so create one to hold descriptive and identifiable data for items available in the game.
    The class did also not contain a method for displaying the item's data.
    Create a function that returns the data declared in the constructor.

--- adventuretutorial/items.py
+++ adventuretutorial/items.py
@@ -1,2 +1,8 @@
 class Item:
-    pass
+    def __init__(self, name, description, value):
+        self.name = name
+        self.description = description
+        self.value = value
+
+    def display_data(self):
+        return "{}, {}, {}".format(self.name, self.description, self.value)
